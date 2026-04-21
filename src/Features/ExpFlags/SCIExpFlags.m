ae3c404c3af3a8884e3e68459b095b66de468711
darthplagueiswise <zelihmannradan@gmail.com>
2026-04-21 17:27:11 -0300

    experimental: persist IGMobileConfig overrides in SCIExpFlags

--- src/Features/ExpFlags/SCIExpFlags.m
+++ src/Features/ExpFlags/SCIExpFlags.m
@@ -1,6 +1,7 @@
 #import "SCIExpFlags.h"
 
 static NSString *const kOverridesKey = @"sci_exp_overrides_by_name";
+static NSString *const kMCOverridesKey = @"sci_exp_overrides_by_pid";
 static NSString *const kCrashCounterKey = @"sci_exp_flags_unstable_launches";
 static const NSInteger kCrashThreshold = 3;
 
@@ -29,6 +30,20 @@ static NSMutableDictionary<NSNumber *, SCIExpMCObservation *> *gMCObs = nil;
     }
 }
 
++ (NSMutableDictionary *)loadMCOverrides {
+    NSDictionary *d = [[NSUserDefaults standardUserDefaults] dictionaryForKey:kMCOverridesKey];
+    return d ? [d mutableCopy] : [NSMutableDictionary dictionary];
+}
+
++ (void)saveMCOverrides:(NSDictionary *)d {
+    NSUserDefaults *ud = [NSUserDefaults standardUserDefaults];
+    if (d.count == 0) {
+        [ud removeObjectForKey:kMCOverridesKey];
+    } else {
+        [ud setObject:d forKey:kMCOverridesKey];
+    }
+}
+
 + (SCIExpFlagOverride)overrideForName:(NSString *)name {
     if (!name.length) return SCIExpFlagOverrideOff;
     NSNumber *n = [self loadOverrides][name];
@@ -52,6 +67,42 @@ static NSMutableDictionary<NSNumber *, SCIExpMCObservation *> *gMCObs = nil;
 
 + (void)resetAllOverrides {
     [[NSUserDefaults standardUserDefaults] removeObjectForKey:kOverridesKey];
+}
+
++ (nullable id)mcOverrideObjectForParamID:(unsigned long long)pid type:(SCIExpMCType)type {
+    NSDictionary *entry = [self loadMCOverrides][@(pid).stringValue];
+    if (![entry isKindOfClass:[NSDictionary class]]) return nil;
+    NSNumber *storedType = entry[@"type"];
+    id value = entry[@"value"];
+    if (![storedType isKindOfClass:[NSNumber class]]) return nil;
+    if (storedType.integerValue != type) return nil;
+    if (!value || value == [NSNull null]) return nil;
+    return value;
+}
+
++ (void)setMCOverrideObject:(nullable id)obj forParamID:(unsigned long long)pid type:(SCIExpMCType)type {
+    NSMutableDictionary *d = [self loadMCOverrides];
+    NSString *key = @(pid).stringValue;
+    if (!obj) {
+        [d removeObjectForKey:key];
+    } else {
+        d[key] = @{@"type": @(type), @"value": obj};
+    }
+    [self saveMCOverrides:d];
+}
+
++ (NSArray<NSNumber *> *)allOverriddenMCParamIDs {
+    NSArray<NSString *> *keys = [[self loadMCOverrides] allKeys];
+    NSMutableArray<NSNumber *> *out = [NSMutableArray arrayWithCapacity:keys.count];
+    for (NSString *k in keys) {
+        unsigned long long v = strtoull(k.UTF8String, NULL, 10);
+        [out addObject:@(v)];
+    }
+    return [out sortedArrayUsingSelector:@selector(compare:)];
+}
+
++ (void)resetAllMCOverrides {
+    [[NSUserDefaults standardUserDefaults] removeObjectForKey:kMCOverridesKey];
 }
 
 + (void)recordExperimentName:(NSString *)name group:(NSString *)group {
@@ -108,8 +159,9 @@ static NSMutableDictionary<NSNumber *, SCIExpMCObservation *> *gMCObs = nil;
 + (BOOL)checkAndHandleCrashLoop {
     NSUserDefaults *ud = [NSUserDefaults standardUserDefaults];
     NSInteger c = [ud integerForKey:kCrashCounterKey] + 1;
-    if (c >= kCrashThreshold && [self loadOverrides].count > 0) {
+    if (c >= kCrashThreshold && ([self loadOverrides].count > 0 || [self loadMCOverrides].count > 0)) {
         [self resetAllOverrides];
+        [self resetAllMCOverrides];
         [ud removeObjectForKey:kCrashCounterKey];
         return YES;
     }
