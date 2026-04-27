571949aeeaa199cab128e63d22f553eb8a935631
euoradan <zelihmannradan@gmail.com>
2026-04-27 17:06:49 -0300

    Forward declare resolver entry type

--- src/Settings/SCIResolverScanner.h
+++ src/Settings/SCIResolverScanner.h
@@ -2,6 +2,8 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
+@class SCIResolverSpecifierEntry;
+
 @interface SCIResolverScanner : NSObject
 
 + (void *)findPattern:(NSString *)patternMask inSegment:(NSString *)segmentName;
