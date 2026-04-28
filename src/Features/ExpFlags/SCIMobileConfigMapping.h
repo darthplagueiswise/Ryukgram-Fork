c1c92f0375571827e8de8a0f6cbe08b6584ef4df
euoradan <zelihmannradan@gmail.com>
2026-04-28 19:53:08 -0300

    Add MobileConfig mapping loader interface

---
+++ src/Features/ExpFlags/SCIMobileConfigMapping.h
@@ -0,0 +1,28 @@
+#import <Foundation/Foundation.h>
+
+@interface SCIMobileConfigMapping : NSObject
+
++ (NSString *)primaryMobileConfigDirectory;
++ (NSString *)primaryIDNameMappingPath;
++ (NSString *)primaryOverridesPath;
++ (NSString *)bundleIGSchemaPath;
+
++ (NSDictionary<NSNumber *, NSDictionary *> *)idNameMapping;
++ (NSDictionary *)mappingForParamID:(unsigned long long)paramID;
++ (NSString *)resolvedNameForParamID:(unsigned long long)paramID;
++ (NSString *)sourceForParamID:(unsigned long long)paramID;
++ (NSString *)mappingStatusLine;
+
++ (NSDictionary *)allOverrides;
++ (NSArray<NSNumber *> *)allOverriddenParamIDs;
++ (id)overrideObjectForParamID:(unsigned long long)paramID typeName:(NSString *)typeName;
++ (void)setOverrideObject:(id)value
+               forParamID:(unsigned long long)paramID
+                 typeName:(NSString *)typeName
+                     name:(NSString *)name;
++ (void)removeOverrideForParamID:(unsigned long long)paramID;
++ (void)resetOverrides;
+
++ (NSArray<NSDictionary *> *)schemaMatchesForQuery:(NSString *)query limit:(NSUInteger)limit;
+
+@end
