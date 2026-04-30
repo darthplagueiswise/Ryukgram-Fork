e81306575e89c1ba40e2b71a99de010e25604ec3
euoradan <zelihmannradan@gmail.com>
2026-04-30 03:42:30 -0300

    Adjust lab inheritance

--- src/Settings/SCIMobileConfigSymbolObserverViewController.h
+++ src/Settings/SCIMobileConfigSymbolObserverViewController.h
@@ -1,4 +1,4 @@
-#import <UIKit/UIKit.h>
+#import "SCIMCFocusedLabViewController.h"
 
-@interface SCIMobileConfigSymbolObserverViewController : UIViewController
+@interface SCIMobileConfigSymbolObserverViewController : SCIMCFocusedLabViewController
 @end
