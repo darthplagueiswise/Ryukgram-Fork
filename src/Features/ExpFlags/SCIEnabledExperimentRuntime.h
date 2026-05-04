ef6ef957318f9aba7bb66d8704cba89bcd4f4daf
euoradan <zelihmannradan@gmail.com>
2026-05-03 21:19:40 -0300

    Expand enabled experiment registry header

--- src/Features/ExpFlags/SCIEnabledExperimentRuntime.h
+++ src/Features/ExpFlags/SCIEnabledExperimentRuntime.h
@@ -1,7 +1,28 @@
 #import <Foundation/Foundation.h>
+#import "SCIExpFlags.h"
 
 @interface SCIEnabledExperimentEntry : NSObject
+@property (nonatomic, copy) NSString *key;
+@property (nonatomic, copy) NSString *className;
+@property (nonatomic, copy) NSString *methodName;
+@property (nonatomic, copy) NSString *source;
+@property (nonatomic, copy) NSString *imageName;
+@property (nonatomic, copy) NSString *typeEncoding;
+@property (nonatomic, assign) BOOL classMethod;
+@property (nonatomic, assign) BOOL defaultKnown;
+@property (nonatomic, assign) BOOL defaultValue;
+@property (nonatomic, assign) NSUInteger hitCount;
+@property (nonatomic, assign) SCIExpFlagOverride savedState;
 @end
 
 @interface SCIEnabledExperimentRuntime : NSObject
++ (void)install;
++ (NSArray<SCIEnabledExperimentEntry *> *)allEntries;
++ (NSArray<SCIEnabledExperimentEntry *> *)filteredEntriesForQuery:(NSString *)query mode:(NSInteger)mode;
++ (void)setSavedState:(SCIExpFlagOverride)state forEntry:(SCIEnabledExperimentEntry *)entry;
++ (SCIExpFlagOverride)savedStateForEntry:(SCIEnabledExperimentEntry *)entry;
++ (NSString *)stateLabelForEntry:(SCIEnabledExperimentEntry *)entry;
++ (NSString *)defaultLabelForEntry:(SCIEnabledExperimentEntry *)entry;
++ (NSString *)summaryTextForEntry:(SCIEnabledExperimentEntry *)entry;
++ (NSUInteger)installedCount;
 @end
