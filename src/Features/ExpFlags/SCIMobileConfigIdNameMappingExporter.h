77f464ff7e697a42d860fff3104771590998ce47
euoradan <zelihmannradan@gmail.com>
2026-05-07 23:17:35 -0300

    alpha3: add MC id-name mapping exporter interface

---
+++ src/Features/ExpFlags/SCIMobileConfigIdNameMappingExporter.h
@@ -0,0 +1,16 @@
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+extern NSString * const SCIMobileConfigIdNameMappingExporterDidUpdateNotification;
+
+@interface SCIMobileConfigIdNameMappingExporter : NSObject
+
++ (NSDictionary *)exportIDNameMappingNow;
++ (NSDictionary *)installNativePathObserver;
++ (NSArray<NSString *> *)candidateIDNameMappingPaths;
++ (nullable NSString *)lastStatusLine;
+
+@end
+
+NS_ASSUME_NONNULL_END
