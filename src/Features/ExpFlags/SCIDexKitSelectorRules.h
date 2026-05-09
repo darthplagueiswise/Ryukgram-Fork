44a835904661e0d7232f80ebe0bff01f1d0291f5
euoradan <zelihmannradan@gmail.com>
2026-05-09 02:39:04 -0300

    Add DexKit selector classifier API

--- src/Features/ExpFlags/SCIDexKitSelectorRules.h
+++ src/Features/ExpFlags/SCIDexKitSelectorRules.h
@@ -7,6 +7,16 @@ NS_ASSUME_NONNULL_BEGIN
 + (NSInteger)curatedScoreForClassName:(NSString *)className selector:(NSString *)selector;
 + (BOOL)isCuratedClassName:(NSString *)className selector:(NSString *)selector;
 + (BOOL)isExcludedClassName:(NSString *)className selector:(NSString *)selector;
+
+// Semantic classifier used by DexKit discovery. It is intentionally metadata-only:
+// scanner/UI can show safer categories without changing existing override keys or
+// hook behavior. Keys returned: semanticCategory, riskLevel, batchForceAllowed,
+// observeRecommended, forceRecommended, classificationReason, familyKey.
++ (NSDictionary<NSString *, id> *)classificationForClassName:(NSString *)className
+                                                    selector:(NSString *)selector
+                                               imageBasename:(NSString *)imageBasename
+                                                typeEncoding:(NSString *)typeEncoding;
++ (NSString *)familyKeyForClassName:(NSString *)className selector:(NSString *)selector;
 @end
 
 NS_ASSUME_NONNULL_END
