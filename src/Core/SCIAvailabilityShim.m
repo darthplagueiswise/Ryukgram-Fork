3a4e770150b1242aee03e27e904e4bf813a7b706
darthplagueiswise <zelihmannradan@gmail.com>
2026-05-13 00:38:38 -0300

    Make availability shim weak for CI

--- src/Core/SCIAvailabilityShim.m
+++ src/Core/SCIAvailabilityShim.m
@@ -1,6 +1,6 @@
 #import <Foundation/Foundation.h>
 
-int __isOSVersionAtLeast(int major, int minor, int subminor) {
+__attribute__((weak)) int __isOSVersionAtLeast(int major, int minor, int subminor) {
     NSOperatingSystemVersion version = [[NSProcessInfo processInfo] operatingSystemVersion];
     if (version.majorVersion != major) return version.majorVersion > major;
     if (version.minorVersion != minor) return version.minorVersion > minor;
