3f4e192ede8db7e385ad8ccb2eb5ffd8cf3e03a5
darthplagueiswise <darthplagueiswise@users.noreply.github.com>
2026-06-22 17:51:48 +0000

    v34.38: restore verified settings UI

--- src/Lock/SCILockGate.m
+++ src/Lock/SCILockGate.m
@@ -93,34 +93,11 @@ static BOOL sciGroupInheritsSettingsLock(NSString *gid) {
 + (void)presentLockedVC:(UIViewController *)contentVC
                 forGroup:(NSString *)groupID
                     from:(UIViewController *)presenter {
-    [self presentLockedVC:contentVC forGroup:groupID from:presenter sourceView:nil];
-}
-
-+ (void)presentLockedVC:(UIViewController *)contentVC
-                forGroup:(NSString *)groupID
-                    from:(UIViewController *)presenter
-              sourceView:(UIView *)sourceView {
     if (!contentVC) return;
-    __weak UIView *weakSource = sourceView;
     [self runGated:groupID from:presenter then:^{
         SCILockedSurfaceNavigationController *nav = [[SCILockedSurfaceNavigationController alloc] initWithRootViewController:contentVC];
         nav.lockGroupID = groupID;
-        BOOL settingsSurface = [groupID isEqualToString:SCILockGroupSettings];
-        nav.modalPresentationStyle = settingsSurface ? UIModalPresentationPageSheet : UIModalPresentationFullScreen;
-        if (settingsSurface) {
-            nav.view.backgroundColor = UIColor.clearColor;
-            nav.view.opaque = NO;
-            contentVC.view.backgroundColor = UIColor.clearColor;
-            contentVC.view.opaque = NO;
-        }
-        if (settingsSurface && @available(iOS 15.0, *)) {
-            UISheetPresentationController *sheet = nav.sheetPresentationController;
-            sheet.detents = @[ UISheetPresentationControllerDetent.largeDetent ];
-            sheet.prefersGrabberVisible = NO;
-            sheet.prefersScrollingExpandsWhenScrolledToEdge = YES;
-            sheet.prefersEdgeAttachedInCompactHeight = YES;
-            sheet.widthFollowsPreferredContentSizeWhenEdgeAttached = NO;
-        }
+        nav.modalPresentationStyle = UIModalPresentationFullScreen;
         SCIUIKit26ApplyContainerBackgroundToViewController(nav);
         SCIConfigureNavigationChromeForGlass(contentVC);
         if (!contentVC.navigationItem.leftBarButtonItem
@@ -131,11 +108,9 @@ static BOOL sciGroupInheritsSettingsLock(NSString *gid) {
                                                                       action:@selector(closeTopMost:)];
             contentVC.navigationItem.leftBarButtonItem = close;
         }
-        UIViewController *top = presenter ?: [self topVC];
-        if (settingsSurface && weakSource && nav.popoverPresentationController) {
-            nav.popoverPresentationController.sourceView = weakSource;
-            nav.popoverPresentationController.sourceRect = weakSource.bounds;
-        }
+        // Guard attaches to the nav (its view hosts every pushed sub-page) so
+        // re-lock can cover inner pages too.
+        UIViewController *top = [self topVC];
         [top presentViewController:nav animated:YES completion:nil];
         [SCILockSurfaceGuard attachToVC:nav forGroup:groupID];
     }];
