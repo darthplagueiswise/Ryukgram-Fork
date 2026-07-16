5c4e5140ca6982e848d0a31dd51323b0aead3ba9
euoradan <zelihmannradan@gmail.com>
2026-07-16 05:41:13 -0300

    Expose native Instagram debug menu opener

--- src/Features/Dogfooding/SCIInternalMenusLauncher.h
+++ src/Features/Dogfooding/SCIInternalMenusLauncher.h
@@ -3,6 +3,7 @@ NS_ASSUME_NONNULL_BEGIN
 @interface SCIInternalMenusLauncher : NSObject
 // Presents IG's own internal/dogfooding menus using validated class-method
 // entrypoints + the live user session/config observed from Instagram runtime.
++ (NSString *)openInstagramDebugMenu;          // -[IGWindow showDebugMenu]
 + (NSString *)openDogfoodingNotesSettings;     // +notesDogfoodingSettingsOpenOnViewController:userSession: (reliable)
 + (NSString *)openDogfoodingSettingsVC;        // +openWithConfig:onViewController:userSession: / initWithConfig:userSession: when config is captured
 + (NSString *)openInternalURLString:(NSString *)urlString; // +[IGURLHandler openInternalURL:...]
