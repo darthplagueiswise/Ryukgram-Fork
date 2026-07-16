4e5b573dce98020a9f054442ab559259b60ba9bf
euoradan <zelihmannradan@gmail.com>
2026-07-16 15:17:10 -0300

    Allow nil debug-menu completion callback

--- src/Features/Dogfooding/SCIInternalMenusLauncher.h
+++ src/Features/Dogfooding/SCIInternalMenusLauncher.h
@@ -4,7 +4,7 @@ NS_ASSUME_NONNULL_BEGIN
 // Presents IG's own internal/dogfooding menus using validated entrypoints and
 // the live user session/config observed from Instagram runtime.
 + (NSString *)openInstagramDebugMenu; // compatibility wrapper
-+ (void)openInstagramDebugMenuWithCompletion:(void (^)(NSString *result))completion;
++ (void)openInstagramDebugMenuWithCompletion:(void (^ _Nullable)(NSString *result))completion;
 + (NSString *)openDogfoodingNotesSettings;
 + (NSString *)openDogfoodingSettingsVC;
 + (NSString *)openInternalURLString:(NSString *)urlString;
