8f1c0998246fa5e913a5b2b74d5293907f6a90b4
euoradan <zelihmannradan@gmail.com>
2026-08-19 13:38:28 -0300

    Add live Developer topic browser

---
+++ src/Debug/RYGDeveloperTopicViewController.h
@@ -0,0 +1,10 @@
+#import <UIKit/UIKit.h>
+#import "RYGDeveloperRuntimeScanner.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface RYGDeveloperTopicViewController : UITableViewController
+- (instancetype)initWithSurface:(RYGDeveloperRuntimeSurface)surface;
+@end
+
+NS_ASSUME_NONNULL_END
