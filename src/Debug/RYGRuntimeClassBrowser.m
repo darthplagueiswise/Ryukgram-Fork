091207eca5d3ddc4042650bda2584ed7829b3b9d
euoradan <zelihmannradan@gmail.com>
2026-08-19 22:05:51 -0300

    Fix ARC-safe class browser buffer

--- src/Debug/RYGRuntimeClassBrowser.m
+++ src/Debug/RYGRuntimeClassBrowser.m
@@ -113,7 +113,7 @@ static NSArray<RYGRuntimeMethodRow *> *RYGRTMethods(Class cls, NSString *imagePa
     if (!classNames.count) {
         int total = objc_getClassList(NULL, 0);
         if (total > 0 && total < 500000) {
-            Class *classes = calloc((size_t)total, sizeof(Class));
+            Class __unsafe_unretained *classes = (Class __unsafe_unretained *)calloc((size_t)total, sizeof(Class));
             int filled = classes ? objc_getClassList(classes, total) : 0;
             for (int i = 0; i < filled; i++) {
                 Class cls = classes[i];
