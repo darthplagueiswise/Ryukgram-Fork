82b0a56a73651c88be396b78107f9d81ce617858
euoradan <zelihmannradan@gmail.com>
2026-08-24 15:09:42 -0300

    Runtime Browser: add C Functions screen header

---
+++ src/Debug/RYGCFunctionsViewController.h
@@ -0,0 +1,7 @@
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+@interface RYGCFunctionsViewController : UITableViewController
+- (instancetype)initWithImagePath:(nullable NSString *)imagePath;
+@end
+NS_ASSUME_NONNULL_END
