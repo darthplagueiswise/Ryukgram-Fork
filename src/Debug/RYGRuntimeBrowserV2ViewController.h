70d6a91d3f19ef2067b049c35562207127ade8f4
euoradan <zelihmannradan@gmail.com>
2026-09-05 03:27:13 -0300

    fix(debug): add robust dyld runtime browser entry

---
+++ src/Debug/RYGRuntimeBrowserV2ViewController.h
@@ -0,0 +1,9 @@
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface RYGRuntimeBrowserV2ViewController : UITableViewController
+- (instancetype)initWithTitle:(NSString *)title initialQuery:(NSString *)initialQuery;
+@end
+
+NS_ASSUME_NONNULL_END
