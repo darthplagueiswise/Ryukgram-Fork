1373e8d94beab63579ba2e33695fee7ac29c2054
euoradan <zelihmannradan@gmail.com>
2026-09-05 03:28:27 -0300

    fix(debug): add static MobileConfig runtime catalog resolver

---
+++ src/Features/ExpFlags/RYGMobileConfigStaticRuntimeCatalog.h
@@ -0,0 +1,9 @@
+#import <Foundation/Foundation.h>
+
+@class RYGMCConfig;
+
+NS_ASSUME_NONNULL_BEGIN
+
+NSArray<RYGMCConfig *> *RYGMCStaticRuntimeConfigs(NSDictionary<NSNumber *, NSDictionary *> *nameCatalog);
+
+NS_ASSUME_NONNULL_END
